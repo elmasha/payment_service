670b39e5aba7885f1ba69d48f5d5b900144f60d3
intec-inc <connect.inkluzon@gmail.com>
2025-09-28 20:36:26 +0300

    SDSSDSD

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,15 @@ module.exports = {
   apps: [
     {
       name: "yayalink",
-      script: "yayalink/server.js",  // change if your entry file differs
-      env: { PORT: 3000 }
+      script: "yayalink.js",
     },
     {
       name: "meatpro",
-      script: "meatpro/server.js",  // change if your entry file differs
-      env: { PORT: 3001 }
+      script: "meatpro.js",
     },
     {
-      name: "nginx",
-      script: "/usr/sbin/nginx",
-      args: ["-g", "daemon off;"]
-    }
-  ]
+      name: "gateway",
+      script: "gateway.js",
+    },
+  ],
 };
