f8cb27b2feafbc8fe93c4853eaeaa69f0e455078
intec-inc <connect.inkluzon@gmail.com>
2025-09-28 22:31:51 +0300

    SDSSDSD

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,22 +3,32 @@ module.exports = {
     {
       name: "yayalink",
       script: "yayalink.js",
+      node_args: "--trace-deprecation",
+      watch: false
     },
     {
       name: "meatpro",
       script: "meatpro.js",
+      node_args: "--trace-deprecation",
+      watch: false
     },
     {
       name: "gateway",
       script: "gateway.js",
+      node_args: "--trace-deprecation",
+      watch: false
     },
     {
       name: "dukalink",
       script: "dukalink.js",
+      node_args: "--trace-deprecation",
+      watch: false
     },
      {
       name: "swiftgas",
       script: "swiftgas.js",
+      node_args: "--trace-deprecation",
+      watch: false
     },
   ],
 };
